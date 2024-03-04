d14379ebc529ceabad7b35ace3b61870dcb68d1f
Kevin Browning <kbrowning1996@gmail.com>
2024-03-04 15:06:17 -0500

    added functionality to be able to enter the code and get an error if wrong

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 2. Create string with assigned message
 3. Create different variables for number combination
 4. Create alert for HTML page with combination
+5. See if I can try anything else after main project was done
  */
 
 // test that js sheet is linked
@@ -33,4 +34,22 @@ const thirdNum = 40 - 1;
 console.log(thirdNum);
 
 // create an alert with the vault combo
-alert(firstNum + "-" + secondNum + "-" + thirdNum);
+alert(`Here is the vault code ` + firstNum + "-" + secondNum + "-" + thirdNum);
+
+/* Extra practice after the main prject was done */
+
+// Make a variable for the correct combination
+const correctNumber = (`${firstNum}-${secondNum}-${thirdNum}`);
+
+// test that it works
+console.log(correctNumber);
+
+// make a prompt alert for thm to enter the vault combo they just saw
+const enterCombo = prompt("Please enter the vault combo: ");
+
+// Was able toresearch and see how to make it so that they get an error if they enter the wrong id
+if (enterCombo === correctNumber) {
+  alert(`Welcome`);
+} else {
+  alert(`Wrong Password`);
+}

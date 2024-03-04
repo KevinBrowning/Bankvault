13108130c2c2d60ff4ebf854661dac43faab13b7
Kevin Browning <kbrowning1996@gmail.com>
2024-03-04 14:41:49 -0500

    Made changes to JS page

--- index.js
+++ index.js
@@ -7,21 +7,30 @@
 
 // test that js sheet is linked
 console.log(`Hello`);
+
 // create string that says You have received this message because you have been chosen to open an important vault. Here is the secret combination:
 const str1 = `You have received this message because you have been chosen to open an important vault. Here is the secret combination:`;
+
 // Test that str1 works 
 console.log(str1);
+
 // add variable one that shows number 10
 const firstNum = 20 / 2;
+
 // test that firstNum works 
 console.log(firstNum);
+
 // add variable two thats shows number 40
 const secondNum = 20 + 20;
+
 // test that secondNum worked 
 console.log(secondNum);
+
 // add varibale two thats shows number 39
 const thirdNum = 40 - 1;
+
 // test that thirdNum worked 
 console.log(thirdNum);
+
 // create an alert with the vault combo
 alert(firstNum + "-" + secondNum + "-" + thirdNum);
